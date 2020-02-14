8d8bfc35a9396942e40da7d3aaff3dcc29f5e724
Taylor Lowery <t.lowery@rtisystems.com>
2020-02-14 08:08:22 -0800

    added package godoc comment on go two-fer

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -1,8 +1,10 @@
+// Package twofer provides a method for checking a name
+// and returning a message based on that name
 package twofer
 
 // ShareWith returns "one for X, one for me"
 func ShareWith(name string) string {
-	if len(name) == 0 {
+	if name == "" {
 		name = "you"
 	}
 
