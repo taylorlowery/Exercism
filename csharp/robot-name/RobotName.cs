2ca5ac44298a4e2e615a3ec24d22ac8064c60f8f
Taylor Lowery <t.lowery@rtisystems.com>
2020-02-24 08:39:24 -0800

    csharp robot-name

--- csharp/robot-name/RobotName.cs
+++ csharp/robot-name/RobotName.cs
@@ -3,48 +3,92 @@ using System.Collections.Generic;
 
 public static class RobotRegistry
 {
-    public static int NameLoopCount = 0;
-    public static HashSet<string> TheNamesOfAllRobots = new HashSet<string>();
+    private static Random _rnd = new Random();
+    private static HashSet<string> TheNamesOfAllRobots = new HashSet<string>();
+    
+    public static string GenerateNewRobot(){
+        string name = GenerateRobotName();
+        while(!TheNamesOfAllRobots.Add(name)){
+            name = GenerateRobotName();
+        }
+        return name;
+    }
+
+    private static string GenerateRobotName(){
+        char a = GetRandomCharacter();
+        char b = GetRandomCharacter();
+        int digits = _rnd.Next(0, 1000);
+        string name = string.Format("{0}{1}{2:000}", a, b, digits);
+        return name;
+
+
+        char GetRandomCharacter() => (char)_rnd.Next('A', 'Z' + 1);
+    }
+
+    public static void RemoveRobotFromRegistry(Robot robot)
+    {
+        // attempt to remove robot's name from registry whether or not it's present
+        // we could track this bool to see if this was a rogue, unregistered robot, etc. 
+        bool removed = TheNamesOfAllRobots.Remove(robot.Name);
+        // regardless, wipe robot's name and deactivate it.
+        robot.DisableRobot();
+    }
 }
 
 public class Robot
 {
+    private bool _active;
     private string _name;
-
-    private Random _rnd = new Random();
-
+    public string Name => _name;
+    public bool Active => _active;
+    
     public Robot()
     {
         Reset();
     }
-
-    public string Name
+    
+    public void Reset()
     {
-        get
-        {
-            if (String.IsNullOrWhiteSpace(_name))
-            {
-                Reset();
-            }
+        // remove robot's name from list of existing robots
+        RobotRegistry.RemoveRobotFromRegistry(this);
+        // wipe current robot's memory (delete name)
+        MemoryWipe();
+        // generate a new name and assign it to the current robot. 
+        _name = RobotRegistry.GenerateNewRobot();
+        // activate the robot
+        ActivateRobot();
+    }
 
-            return _name;
+    // These methods are sort of just for fun. 
+    // I was thinking that the Robot Registry Bureau wouldn't want rogue robots to be active without being registered, 
+    // so we prevent it from being active without the name
+    // We can assume in a real robot there would be other methods that would only be possible if the robot were active
+
+    // prevent the robot from being activated without being registered
+    public void ActivateRobot()
+    {
+        if (!String.IsNullOrWhiteSpace(this.Name))
+        {
+            _active = true;
         }
     }
 
-    public void Reset()
+    // Turn the robot on and off
+    public void DeactivateRobot()
     {
-        char a = (char)_rnd.Next(65, 91);
-        char b = (char)_rnd.Next(65, 91);
-        string digits = _rnd.Next(100, 1000).ToString();
-        string name = $"{a}{b}{digits}";
-        if (RobotRegistry.TheNamesOfAllRobots.Add(name))
-        {
-            _name = name;
-        }
-        else
-        {
-            RobotRegistry.NameLoopCount++;
-            Reset();
-        }
+        _active = false;
+    }
+
+    // Remove the robot's name and deactivate it.
+    public void DisableRobot()
+    {
+        MemoryWipe();
+        DeactivateRobot();
+    }
+
+    // wipe the robot's memory (remove its name)
+    public void MemoryWipe()
+    {
+        _name = String.Empty;
     }
 }
