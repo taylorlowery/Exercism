7922db4c1523761dab8cce89d087bd6d1b5c18ab
Taylor Lowery <t.lowery@rtisystems.com>
2020-02-25 08:29:13 -0800

    Added linq-based solution to csharp protein-translation

--- csharp/protein-translation/ProteinTranslation.cs
+++ csharp/protein-translation/ProteinTranslation.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 public static class ProteinTranslation
 {    
-    public static string _proteinSwitch(string strand)
+    public static string TranslateSubStrandToProtein(string strand)
     {
         switch (strand)
         {
@@ -39,12 +39,18 @@ public static class ProteinTranslation
 
     public static string[] Proteins(string strand)
     {
+        //return ConvertStrandToProteinsWithLoop(strand);
+        return ConvertStrandToProteinsWithLinq(strand);
+    }
+
+    public static string[] ConvertStrandToProteinsWithLoop(string strand)
+    {
         HashSet<string> proteins = new HashSet<string>();
 
-        for (int i = 0; i <= strand.Length-3; i+=3)
+        for (int i = 0; i <= strand.Length - 3; i += 3)
         {
             string subStrand = strand.Substring(i, 3);
-            var protein = _proteinSwitch(subStrand);
+            var protein = TranslateSubStrandToProtein(subStrand);
             if (protein.Equals("STOP"))
             {
                 break;
@@ -53,5 +59,17 @@ public static class ProteinTranslation
         }
 
         return proteins.ToArray();
+    }
+
+    // pretty much copied from michalszewa's solution at https://exercism.io/tracks/csharp/exercises/protein-translation/solutions/821e39ee1341420aafb48140240c6c1e
+    public static string[] ConvertStrandToProteinsWithLinq(string strand)
+    {
+        return strand
+                .Select((s, i) => i)
+                .Where(i => i % 3 == 0)
+                .Select(i => strand.Substring(i, 3))
+                .TakeWhile(s => TranslateSubStrandToProtein(s) != "STOP")
+                .Select(s => TranslateSubStrandToProtein(s))
+                .ToArray();
     }
 }
