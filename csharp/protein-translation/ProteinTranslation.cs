03f1205dca6f191f6bf2379dd73afb6c8ddd7a82
Taylor Lowery <t.lowery@rtisystems.com>
2020-02-25 07:42:38 -0800

    csharp protein-translation initial solution

--- csharp/protein-translation/ProteinTranslation.cs
+++ csharp/protein-translation/ProteinTranslation.cs
@@ -1,32 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class ProteinTranslation
-{
-    public static readonly Dictionary<string, string> _proteins = new Dictionary<string, string>
+{    
+    public static string _proteinSwitch(string strand)
     {
-        { "AUG", "Methionine" },
-        { "UUU", "Phenylalanine" },
-        { "UUC", "Phenylalanine" },
-        { "UUA", "Leucine" },
-        { "UUG", "Leucine" },
-        { "UCU", "Serine" },
-        { "UCC", "Serine" },
-        { "UCA", "Serine" },
-        { "UCG", "Serine" },
-        { "UAU", "Tyrosine" },
-        { "UAC", "Tyrosine" },
-        { "UGU", "Cysteine" },
-        { "UGC", "Cysteine" },
-        { "UGG", "Tryptophan" },
-        { "UAA", "STOP" },
-        { "UAG", "STOP" },
-        { "UGA", "STOP" },
-
-    };
+        switch (strand)
+        {
+            case "AUG":
+                return "Methionine";
+            case "UUU":
+            case "UUC":
+                return "Phenylalanine";
+            case "UUA":
+            case "UUG":
+                return "Leucine";
+            case "UCU":
+            case "UCC":
+            case "UCA":
+            case "UCG":
+                return "Serine";
+            case "UAU":
+            case "UAC":
+                return "Tyrosine";
+            case "UGU":
+            case "UGC":
+                return "Cysteine";
+            case "UGG":
+                return "Tryptophan";
+            case "UAA":
+            case "UAG":
+            case "UGA":
+            default:
+                return "STOP";
+        }
+    }
 
     public static string[] Proteins(string strand)
     {
-        throw new NotImplementedException();
+        HashSet<string> proteins = new HashSet<string>();
+
+        for (int i = 0; i <= strand.Length-3; i+=3)
+        {
+            string subStrand = strand.Substring(i, 3);
+            var protein = _proteinSwitch(subStrand);
+            if (protein.Equals("STOP"))
+            {
+                break;
+            }
+            proteins.Add(protein);
+        }
+
+        return proteins.ToArray();
     }
 }
