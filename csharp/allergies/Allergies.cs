1b7ffbfde81f451d5095660bd11eed5b62d8b263
Taylor Lowery <admiral.taylor@gmail.com>
2020-02-28 15:22:29 -0800

    csharp allergies, working on dart allergies

--- csharp/allergies/Allergies.cs
+++ csharp/allergies/Allergies.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
+[Flags]
 public enum Allergen
 {
     Eggs = 1,
@@ -13,21 +14,16 @@ public enum Allergen
     Pollen = 64,
     Cats = 128
 }
+
 public class Allergies
 {
     IEnumerable<Allergen> Allergens = (IEnumerable<Allergen>)Enum.GetValues(typeof(Allergen));
 
     int _mask { get; set; }
-    int Mask => _mask;
+    byte Mask => (byte)_mask;
 
     public Allergies(int mask)
     {
-        // if the mask is higher than the sum of all allergens
-        if (mask > Allergens.Sum(a => (int)a))
-        {
-            mask = mask - HighestPowerOfTwoLessThanNum(mask);
-        }
-
         _mask = mask;
     }
 
@@ -35,26 +31,63 @@ public class Allergies
     public bool IsAllergicTo(Allergen allergen) => this.List().Contains(allergen);
 
     // List of allergens to which person is allergic
-    public Allergen[] List()
+    public IEnumerable<Allergen> List()
     {
-        var allergens = new List<Allergen>();
-
-        int mask = Mask;
-
-        foreach (var allergen in Allergens.OrderByDescending(a => (int)a))
+        foreach (var allergen in Allergens)
         {
-            if (mask >= (int)allergen)
+            if ((Mask & (byte)allergen) > 0)
             {
-                allergens.Add(allergen);
-                mask -= (int)allergen;
+                yield return allergen;
             }
         }
-        
-        return allergens.OrderBy(a => (int)a).ToArray();
     }
-
-    public int HighestPowerOfTwoLessThanNum(int num) => (int)(Enumerable.Range(0, num)
-                                                            .Select(s => Math.Pow(2, s))
-                                                                .TakeWhile(p => p < num)
-                                                                    .Max());
 }
+
+
+// Below here is my original solution, which does not use bitwise operators
+// I keep it here as a cautionary tale for those who follow
+//public class Allergies
+//{
+//    IEnumerable<Allergen> Allergens = (IEnumerable<Allergen>)Enum.GetValues(typeof(Allergen));
+
+//    int _mask { get; set; }
+//    int Mask => _mask;
+
+//    public Allergies(int mask)
+//    {
+//        // if the mask is higher than the sum of all allergens
+//        if (mask > Allergens.Sum(a => (int)a))
+//        {
+//            mask = mask - HighestPowerOfTwoLessThanNum(mask);
+//        }
+
+//        _mask = mask;
+//    }
+
+//    // determine if person has allergies
+//    public bool IsAllergicTo(Allergen allergen) => this.List().Contains(allergen);
+
+//    // List of allergens to which person is allergic
+//    public Allergen[] List()
+//    {
+//        var allergens = new List<Allergen>();
+
+//        int mask = Mask;
+
+//        foreach (var allergen in Allergens.OrderByDescending(a => (int)a))
+//        {
+//            if (mask >= (int)allergen)
+//            {
+//                allergens.Add(allergen);
+//                mask -= (int)allergen;
+//            }
+//        }
+
+//        return allergens.OrderBy(a => (int)a).ToArray();
+//    }
+
+//    public int HighestPowerOfTwoLessThanNum(int num) => (int)(Enumerable.Range(0, num)
+//                                                            .Select(s => Math.Pow(2, s))
+//                                                                .TakeWhile(p => p < num)
+//                                                                    .Max());
+//}
