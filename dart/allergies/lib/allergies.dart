0e8601dbaa1d5c3a718544fecf4390489d2abd1c
Taylor Lowery <admiral.taylor@gmail.com>
2020-02-28 15:54:11 -0800

    dart allergies completed with bitwise operators

--- dart/allergies/lib/allergies.dart
+++ dart/allergies/lib/allergies.dart
@@ -1,3 +1,28 @@
 class Allergies {
-  // Put your code here
+  Map<String, int> Allergens = {
+    'eggs': 1,
+    'peanuts': 2,
+    'shellfish': 4,
+    'strawberries': 8,
+    'tomatoes': 16,
+    'chocolate': 32,
+    'pollen': 64,
+    'cats': 128
+  };
+
+  bool allergicTo(String allergen, int score) {
+    return list(score).contains(allergen);
+  }
+
+  List<String> list(int score) {
+    List<String> allergens = [];
+
+    Allergens.forEach((k, v) {
+      if (v & score > 0) {
+        allergens.add(k);
+      }
+    });
+
+    return allergens;
+  }
 }
