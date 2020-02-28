13de3a30426e764d2d05d2b441d6a1904bfa6852
Taylor Lowery <admiral.taylor@gmail.com>
2020-02-27 20:55:00 -0800

    dart nth-prime added comments and improved check for prime

--- dart/nth-prime/lib/nth_prime.dart
+++ dart/nth-prime/lib/nth_prime.dart
@@ -1,27 +1,35 @@
+import 'dart:math';
+
 class NthPrime {
   int prime(int index) {
+    //  track the number of primes that have been found
+    //  this way we avoid the memory issue of  tracking a huge list of numbers as index -> infinity
     int primesFound = 0;
 
     if (index < 1) {
       throw new ArgumentError('There is no zeroth prime');
     }
 
+    // as this counter increases, we'll check if it's prime, and count the number of primes
+    // we can start at 1, because the first prime is 2.
     int counter = 1;
     while (primesFound < index) {
       counter++;
       if (isPrime(counter)) {
-        primesFound++;
+        primesFound++; // if the counter is prime, increase the count of primes found
       }
     }
-    return counter;
+    return counter; // once you've found the nth prime, return the counter
   }
 
   bool isPrime(int number) {
-    for (int i = 2; i < number; i++) {
+    // unfortunately have to loop from 1 up to see if number has any divisors
+    // however, you only have to iterate up to sqrt of number
+    for (int i = 2; i <= sqrt(number); i++) {
       if (number % i == 0) {
-        return false;
+        return false; // if any number divides evenly, not prime
       }
     }
-    return true;
+    return true; // woohoo, it's prime!
   }
 }
