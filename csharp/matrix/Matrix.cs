6bc4ddb6851e184e383af702cdddc2694720b121
Taylor Lowery <admiral.taylor@gmail.com>
2020-03-01 15:52:37 -0800

    csharp matrix

---
+++ csharp/matrix/Matrix.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Matrix
+{
+    private IEnumerable<IEnumerable<int>> _matrix { get; set; }
+
+    public Matrix(string input) => _matrix = input.Split('\n')
+                                        .Select(r => r.Split(' '))
+                                        .Select(r => r.Select(int.Parse));
+
+
+    public int Rows => _matrix.Count();
+
+    // for the purposes of this exercise, I'll assume they're all the same length
+    public int Cols => _matrix.First().Count();
+
+    public IEnumerable<int> Row(int row) => _matrix.ElementAt(row - 1).ToArray();
+
+    public IEnumerable<int> Column(int col) => _matrix.Select(r => r.ElementAt(col - 1)).ToArray();
+
+}
