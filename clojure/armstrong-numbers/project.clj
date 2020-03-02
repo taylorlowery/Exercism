15ccd179ac28833d74a6663f4badd7ff969abd64
Taylor Lowery <t.lowery@rtisystems.com>
2020-03-02 12:41:07 -0800

    clojure armstrong -- improved solution)

--- clojure/armstrong-numbers/project.clj
+++ clojure/armstrong-numbers/project.clj
@@ -2,4 +2,4 @@
   :description "armstrong-numbers exercise"
   :url "https://github.com/exercism/clojure/tree/master/exercises/armstrong-numbers"
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/math.numeric-tower "0.0.4"]])
+                    [org.clojure/math.numeric-tower "0.0.4"]])
--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -3,9 +3,17 @@
             [clojure.math.numeric-tower :as math]))
 
 (defn num-to-digits [num]
-  (into [] (map #(Integer/parseInt %) (str/split (str num) #""))))
+      (into [] (map #(read-string (str %)) (str num))))
 
 (defn armstrong? [num]
   (let [digits (num-to-digits num)
         exponent (count digits)]
     (= num (reduce + (map #(math/expt % exponent) digits)))))
+
+(defn armstrong2? [num]
+      (let [digits (num-to-digits num)
+            exponent (count digits)]
+           (= num
+              (->> digits
+                   (map #(math/expt % exponent))
+                   (reduce +)))))
