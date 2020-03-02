0249f1ba109ac9e10f2e12e380dcfdbebedea4d8
Taylor Lowery <t.lowery@rtisystems.com>
2020-03-02 12:42:29 -0800

    clojure armstrong -- improved solution)

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -6,11 +6,6 @@
       (into [] (map #(read-string (str %)) (str num))))
 
 (defn armstrong? [num]
-  (let [digits (num-to-digits num)
-        exponent (count digits)]
-    (= num (reduce + (map #(math/expt % exponent) digits)))))
-
-(defn armstrong2? [num]
       (let [digits (num-to-digits num)
             exponent (count digits)]
            (= num
