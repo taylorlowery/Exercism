4700df0b06b54370db659c36221f7d15f735f5d4
Taylor Lowery <t.lowery@rtisystems.com>
2020-03-03 15:00:36 -0800

    dart luhn

--- dart/luhn/lib/luhn.dart
+++ dart/luhn/lib/luhn.dart
@@ -1,5 +1,50 @@
 class Luhn {
-  bool valid(String cardNumber) {
+  bool valid(String cardNumber){
+    // remove spaces
+    RegExp exp = new RegExp(r"([ ])");
+    cardNumber = cardNumber.replaceAll(exp, '');
+
+    // check for non digit characters
+    RegExp nonDigit = new RegExp(r"\D+");
+    if(nonDigit.hasMatch(cardNumber)){
+      return false;
+    }
+
+    // make sure it's at least two nubmers long
+    if(cardNumber.length < 2){
+      return false;
+    }
+
+    // finally check the number
+    List<int> numbers = cardNumberToList(cardNumber);
+    numbers = luhnizedNumber(numbers);
+    int sum = numbers.fold(0, (a, b) => (a + b));
+    if(sum % 10 == 0){
+      return true;
+    }
     return false;
+  }
+
+  List<int> cardNumberToList(String cardNumber){
+
+    List<int> numbers = [];
+    var stringNums = cardNumber.split('');
+    for (var value in stringNums) {
+      var val = int.parse(value);
+      numbers.add(val);
+    }
+    return numbers;
+  }
+
+  List<int> luhnizedNumber(List<int> numbers){
+    for (int i = numbers.length - 2; i >= 0 ; i--){
+      if(i.isEven){
+        numbers[i] *= 2;
+        if (numbers[i] > 9){
+          numbers[i] -= 9;
+        }
+      }
+    }
+    return numbers;
   }
 }
