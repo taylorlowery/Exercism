bddfee355e9635b6d7520283f73f221f583e41ed
Taylor Lowery <admiral.taylor@gmail.com>
2020-03-09 21:22:12 -0700

    clj two-fer

--- clojure/two-fer/src/two_fer.clj
+++ clojure/two-fer/src/two_fer.clj
@@ -1,7 +1,7 @@
 (ns two-fer)
 
 (defn two-fer
-  ([name]
-    (str "One for " name ", one for me."))
   ([]
-   (two-fer "you")))
+   (two-fer "you"))
+  ([name]
+    (str "One for " name ", one for me.")))
