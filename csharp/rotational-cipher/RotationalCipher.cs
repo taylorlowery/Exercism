3b9ccb532743806da4c75b6b698adcfb17e6342d
Taylor Lowery <t.lowery@rtisystems.com>
2020-03-11 08:31:08 -0700

    csharp rotational cipher

--- csharp/rotational-cipher/RotationalCipher.cs
+++ csharp/rotational-cipher/RotationalCipher.cs
@@ -8,22 +8,18 @@ public static class RotationalCipher
 
     public static string Rotate(string text, int shiftKey)
     {
-        return new string(text.ToCharArray().Select(c => RotateChar(c, shiftKey)).ToArray());
-    }
+        char RotateChar(Char c)
+        {
+            if (!char.IsLetter(c))
+                return c;
+            
+            if (shiftKey % 26 == 0)
+                return c;
 
-    public static Char RotateChar(char letter, int shift)
-    {
-        if (regex.IsMatch(letter.ToString())) {
-            char newChar = (char)(letter + shift);
-
-            if ((letter <= 'z' && 'z' <= newChar) || ('A' <= letter && 'Z' <= newChar))
-            {
-                newChar -= (char)26;
-            }
-
-            return newChar;
+            int baseChar = char.IsLower(c) ? 'a' : 'A';
+            return (char)(baseChar + ((c - baseChar + shiftKey) % 26));            
         }
 
-        return letter;        
+        return string.Concat(text.Select(RotateChar));
     }
 }
