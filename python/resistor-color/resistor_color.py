84850e8e204962d46153182fcee97f8d1a8f303c
Taylor Lowery <t.lowery@rtisystems.com>
2020-03-12 11:18:22 -0700

    python resistor-color initial solution

---
+++ python/resistor-color/resistor_color.py
@@ -0,0 +1,20 @@
+_colors = [
+            "black",
+            "brown",
+            "red",
+            "orange",
+            "yellow",
+            "green",
+            "blue",
+            "violet",
+            "grey",
+            "white",
+        ]
+
+
+def color_code(color):
+    return _colors.index(color)
+
+
+def colors():
+    return _colors
