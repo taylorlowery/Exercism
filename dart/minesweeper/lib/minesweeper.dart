9cce4d581b4e7e86cb46812aa0fc615866bc35a3
Taylor Lowery <admiral.taylor@gmail.com>
2020-03-11 21:02:29 -0700

    dart minesweeper initial solution

--- dart/minesweeper/lib/minesweeper.dart
+++ dart/minesweeper/lib/minesweeper.dart
@@ -1,3 +1,65 @@
 class Minesweeper {
-  // Put your code here.
+  List<List<String>> board = [];
+
+  Minesweeper([List<String> setup]) {
+    setup.forEach((f) {
+      print(f);
+      var subList = f.split('').toList();
+      this.board.add(subList);
+    });
+  }
+
+  bool isMine(int x, int y) => board[x][y] == '*';
+
+  int rowSum(int i, int j) {
+    int sum = 0;
+
+    // j - 1
+    if (j > 0) {
+      if (isMine(i, j - 1)) {
+        sum++;
+      }
+    }
+    // j
+    if (isMine(i, j)) {
+      sum++;
+    }
+
+    // j + 1
+    if (j < this.board[0].length - 1) {
+      if (isMine(i, j + 1)) {
+        sum++;
+      }
+    }
+
+    return sum;
+  }
+
+  List<String> annoted() {
+    List<List<String>> parsedBoard = this.board;
+
+    for (int i = 0; i < this.board.length; i++) {
+      for (int j = 0; j < this.board[i].length; j++) {
+        if (parsedBoard[i][j] == ' ') {
+          int sum = 0;
+
+          // i - 1
+          if (i > 0) {
+            sum += rowSum(i - 1, j);
+          }
+
+          // i
+          sum += rowSum(i, j);
+          // i + 1
+          if (i < this.board.length - 1) {
+            sum += rowSum(i + 1, j);
+          }
+
+          parsedBoard[i][j] = sum > 0 ? sum.toString() : ' ';
+        }
+      }
+    }
+
+    return parsedBoard.map((l) => (l.join())).toList();
+  }
 }
