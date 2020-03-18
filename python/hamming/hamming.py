58605cf04c0ed8aab5a26c509d3a9cfdb35c7d8f
Taylor Lowery <admiral.taylor@gmail.com>
2020-03-17 20:00:42 -0700

    python hamming

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,5 +1,4 @@
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("The strands must be of the same length.")
-    stuff = [(x, y) for (x, y) in zip(list(strand_a), list(strand_b)) if x != y]
-    return len(stuff)
+    return len([(x, y) for (x, y) in zip(list(strand_a), list(strand_b)) if x != y])
